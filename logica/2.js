c8a56ab6940222c48142f22a2a2fa97f26d04388
Florencia <florenciasoldavinivarela@gmail.com>
2024-05-08 21:41:19 -0300

    Ejercicio de lógica 2 resuelto

--- logica/2.js
+++ logica/2.js
@@ -13,5 +13,29 @@
  */
 
 const t9Keyboard = value => {
-  return
+  const t9Map = {
+    '2': ['A', 'B', 'C'],
+    '3': ['D', 'E', 'F'],
+    '4': ['G', 'H', 'I'],
+    '5': ['J', 'K', 'L'],
+    '6': ['M', 'N', 'O'],
+    '7': ['P', 'Q', 'R', 'S'],
+    '8': ['T', 'U', 'V'],
+    '9': ['W', 'X', 'Y', 'Z']
+  };
+
+  let blocks = value.split('-');
+  let result = '';
+
+  blocks.forEach(block => { 
+    let number = block[0];
+    let timesPressed = block.length;
+    let index = timesPressed - 1;
+
+    result += t9Map[number][index % t9Map[number].length];
+  });
+
+  return result;
 }
+
+console.log(t9Keyboard('6-666-88-777-33-3-33-888'));
