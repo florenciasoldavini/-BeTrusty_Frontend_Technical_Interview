9dd91a6da0d22c6bcc4315cddc46d9fa2d9fc1d5
Florencia <florenciasoldavinivarela@gmail.com>
2024-05-15 18:52:53 -0300

    se agrega componente ArrowIcon

---
+++ app/my-app/src/components/icons/ArrowIcon.tsx
@@ -0,0 +1,16 @@
+const ArrowIcon = () => {
+  <svg
+    width="26"
+    height="26"
+    viewBox="0 0 26 26"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M4.30878 10.5663V14.7588H13.7778L10.0954 18.4273L12.6415 20.9638L20.9742 12.6626L12.6415 4.36133L10.0954 6.89782L13.7778 10.5663H4.30878Z"
+      fill="white"
+    />
+  </svg>;
+};
+
+export default ArrowIcon;
