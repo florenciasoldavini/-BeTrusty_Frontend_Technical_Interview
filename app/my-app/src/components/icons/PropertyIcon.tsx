b211691fd4289ba22e41bc6c5fe5c81ccc65530c
Florencia <florenciasoldavinivarela@gmail.com>
2024-05-15 13:43:18 -0300

    creacion de componentes para los iconos

---
+++ app/my-app/src/components/icons/PropertyIcon.tsx
@@ -0,0 +1,16 @@
+const PropertyIcon = () => (
+  <svg
+    width="30"
+    height="30"
+    viewBox="0 0 30 30"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12 21.25V13.75H17V21.25H23.25V11.25H27L14.5 0L2 11.25H5.75V21.25H12Z"
+      fill="#33EAFF"
+    />
+  </svg>
+);
+
+export default PropertyIcon;
