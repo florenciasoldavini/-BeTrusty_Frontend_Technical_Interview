d397f3c8722b156ac0d18cb47901463ed6f7fc3c
Florencia <florenciasoldavinivarela@gmail.com>
2024-05-16 02:28:58 -0300

    mejoras en la nav bar se ocultan los iconos que no deben mostrarse en el formato mobile

--- app/my-app/src/components/Navbar.tsx
+++ app/my-app/src/components/Navbar.tsx
@@ -11,18 +11,36 @@ import KeyIcon from "./icons/KeyIcon";
 
 const Navbar = () => {
   return (
-    <nav className="fixed flex flex-row h-24 w-full items-center justify-between md:flex-col md:h-full md:w-24 md:left-0 bottom-0 bg-black">
-      <MenuIcon className="m-2 sm:hidden" />
-      <div className="flex flex-row md:flex-col">
-        <HomeIcon className="m-2" />
-        <IdentificationIcon className="m-2" />
-        <DashboardIcon className="m-2" />
-        <QrCodeIcon className="m-2" />
-        <KeyIcon className="m-2" />
-        <NotificationsIcon className="m-2" />
+    <nav className="fixed flex flex-row h-24 w-full items-center justify-center md:justify-between md:flex-col md:h-full md:w-24 md:left-0 bottom-0 bg-black">
+      <div className="flex md:flex-col">
+        <div className="m-6 hidden sm:block">
+          <MenuIcon className="m-6 " />
+        </div>
+        <div className="flex flex-row md:flex-col items-center justify-center">
+          <div className="m-2">
+            <HomeIcon />
+          </div>
+          <div className="m-2 hidden sm:block">
+            <IdentificationIcon />
+          </div>
+          <div className="m-2">
+            <DashboardIcon />
+          </div>
+          <div className="m-2">
+            <QrCodeIcon />
+          </div>
+          <div className="m-2 hidden sm:block">
+            <KeyIcon />
+          </div>
+          <div className="m-2">
+            <NotificationsIcon />
+          </div>
+        </div>
       </div>
       <Link href="/configuracion">
-        <SettingsIcon className="m-2" />
+        <div className="m-2">
+          <SettingsIcon />
+        </div>
       </Link>
     </nav>
   );
